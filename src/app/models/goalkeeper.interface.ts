1cb20c3550b333720c10bd554631917c21d68e01
PabloDefelitto <pdefelitto@gmail.com>
2024-06-15 15:24:18 +0200

    adde json and interface

---
+++ src/app/models/goalkeeper.interface.ts
@@ -0,0 +1,16 @@
+export interface Goalkeeper {
+    name: string;
+    position: string;
+    number: number;
+    nationality: string;
+    age: number;
+    height: string;
+    weight: string;
+    seasons: number;
+    matches: number;
+    clean_sheets: number;
+    saves: number;
+    info: string;
+    photo: string;
+  }
+  
---
+++ src/app/models/player.interface.ts
@@ -0,0 +1,16 @@
+export interface Player {
+    name: string;
+    position: string;
+    number: number;
+    nationality: string;
+    age: number;
+    height: string;
+    weight: string;
+    seasons: number;
+    matches: number;
+    goals: number;
+    assists: number;
+    info: string;
+    photo: string;
+  }
+  
